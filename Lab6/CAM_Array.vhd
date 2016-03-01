7debc64de00b609449020bda9993f502af8bc3e6
btstevenson <bstevo30@gmail.com>
2016-03-01 14:08:36 -0800

    Functionality for 0 row CAM cell
    Used conditional generate in order to initialize the zero row to the
    clock. Fixed the cam array file to use the cam row. Need to implement a
    way in CAM_Row to detect what cell the row will be using.

--- Lab6/CAM_Array.vhd
+++ Lab6/CAM_Array.vhd
@@ -15,88 +15,37 @@ entity CAM_Array is
            decoded_match_address : out  STD_LOGIC_VECTOR (CAM_DEPTH-1 downto 0));
 end entity ;
 
-architecture Behavioral of CAM_Row is
+architecture Behavioral of CAM_Array is
 
+	component CAM_Row is
+		Generic (CAM_WIDTH : integer := 8);
+		Port (
+			clk : in  STD_LOGIC;
+         rst : in  STD_LOGIC;
+         we : in  STD_LOGIC;
+         search_word : in  STD_LOGIC_VECTOR (CAM_WIDTH-1 downto 0);
+         dont_care_mask : in  STD_LOGIC_VECTOR (CAM_WIDTH-1 downto 0);
+         row_match : out  STD_LOGIC
+		);	
+	end component;
 
-component BCAM_Cell is
-    Port ( clk : in  STD_LOGIC;
-           rst : in  STD_LOGIC;
-           we : in  STD_LOGIC;
-           cell_search_bit : in  STD_LOGIC;
-			  cell_match_bit_in : in  STD_LOGIC ;
-           cell_match_bit_out : out  STD_LOGIC);
-end component ;
-
-component STCAM_Cell is
-    Port ( clk : in  STD_LOGIC;
-           rst : in  STD_LOGIC;
-           we : in  STD_LOGIC;
-           cell_search_bit : in  STD_LOGIC;
-           cell_dont_care_bit : in  STD_LOGIC;
-			  cell_match_bit_in : in  STD_LOGIC ;
-           cell_match_bit_out : out  STD_LOGIC);
-end component ;
-
-component TCAM_Cell is
-    Port ( clk : in  STD_LOGIC;
-           rst : in  STD_LOGIC;
-           we : in  STD_LOGIC;
-           cell_search_bit : in  STD_LOGIC;
-           cell_dont_care_bit : in  STD_LOGIC;
-			  cell_match_bit_in : in  STD_LOGIC ;
-           cell_match_bit_out : out  STD_LOGIC);
-end component ;
 
 
 Signal cell_match_bit_internal : STD_LOGIC_VECTOR(CAM_DEPTH downto 0);
 
 
 begin
--- Connect the CAM cells here
+-- Connect the CAM Row cells here
 
-	cell_match_bit_internal(0) <= '1';
-	
-	BCells: for N in 0 to CAM_DEPTH-1 generate
-	myBCAM: BCAM_Cell port map(
-		if N = 0 then
-			clock => clock(N);
-		end if;
-		rst => rst(N);
-		we => we_decoded_row_address(N);
-		--cell_search_bit => cell_search_bit(N);
-		cell_search_bit => search_word(N);
-		cell_match_bit_in => cell_match_bit_internal(N);
-		cell_match_bit_out => cell_match_bit_internal(N+1) );
-		
-	end generate;
+	--RowCells : for N in 0 to CAM_DEPTH-1 generate
+		--Rows : CAM_Row generic map(CAM_WIDTH => CAM_WIDTH) port map (
+			--clk => clk,
+			--rst => rst,
+			--we => we_decoded_row_address(N),
+			--search_word => search_word,
+			--dont_care_mask => dont_care_mask,
+			--row_match => decoded_match_address(N)
+		--);
+	--end generate;
 
-	STCells: for N in 0 to CAM_DEPTH-1 generate
-	mySTCAM: STCAM_Cell port map(
-		if N = 0 then
-			clock => clock(N);
-		end if;
-		rst => rst(N);
-		we => we_decoded_row_address(N);
-		cell_dont_care_bit => dont_care_mask(N);
-		cell_search_bit => search_word(N);
-		cell_match_bit_in => cell_match_bit_internal(N);
-		cell_match_bit_out => cell_match_bit_internal(N+1) );
-		
-	end generate;
-	
-	TCells: for N in 0 to CAM_DEPTH-1 generate
-	myTCAM: TCAM_Cell port map(
-		if N = 0 then
-			clock => clock(N);
-		end if;
-		rst => rst(N);
-		we => we_decoded_row_address(N);
-		cell_dont_care_bit => dont_care_mask(N);
-		cell_search_bit => search_word(N);
-		cell_match_bit_in => cell_match_bit_internal(N);
-		cell_match_bit_out => cell_match_bit_internal(N+1) );
-		
-	end generate;
-
-	decoded_match_address <= cell_match_bit_internal(CAM_DEPTH);
 end Behavioral;
